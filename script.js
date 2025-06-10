fef2c576af93cf75c47e362c11f9e9fb7916b3d5
Sandilem7789 <sandilem7789@gmail.com>
2025-06-10 18:39:45 +0200

    fixing the contacts css

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ function fetchContacts() {
     fetch(rootPath + "controller/get-contacts/").then(function (response) {
         return response.json();
     }).then(function (data) {
-        //displayOutput(data);
+        displayOutput(data);
         console.log(data);
-        
     })
+}
+
+function displayOutput(data) {
+    output = `<table class="contacts-table>`;
+
+    for (a in data) {
+        console.log(data[a].firstname + " " + data[a].lastname);
+        output += `
+            <tr>
+                <td><img src="${rootPath}controller/uploads/${data[a].avatar}"></td>
+                <td><h5>${data[a].firstname}</h5></td>
+                <td><h5>${data[a].lastname}</h5></td>
+            </tr>
+        `
+    }
+    output += `</table>`;
+    document.getElementById("table").innerHTML = output;
 }
