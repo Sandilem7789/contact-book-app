598dbc65403f879aeadc8bafb60fdf55692b79c2
Sandilem7789 <sandilem7789@gmail.com>
2025-06-19 11:30:55 +0200

    getContacts Function

--- edit-contact.js
+++ edit-contact.js
@@ -1,5 +1,8 @@
 var id = getId();                                       // Call the function to get the ID from the URL
-console.log("The ID is: " + id);                     // Log the ID to the console
+console.log("The ID is: " + id);                        // Log the ID to the console
+
+//an event listener for the homelink button
+document.getElementById("homeLink").addEventListener("click", homeLink);
 
 
 //we create a fuction for editing contacts using the id from the API
@@ -9,4 +12,22 @@ function getId() {
     var id = url.slice(pos + 1);                        // Extract the substring starting from the position after "=" to get the ID
     return id;                                          // Return the extracted ID
 
+}
+
+//we create a function to get the contact data from the API
+function getContact() {
+    let urlString = "controller/get-contacts/?id"      // Define the base URL for the API endpoint to get contact data
+
+    fetch(rootPath + urlString + id)                   // Make a GET request to the API endpoint with the contact ID
+        .then(function (response) {
+            return response.json();                    // Parse the JSON response
+        })
+        .then(function (data) {
+            console.log(data);                         // Log the data to the console
+            //displayContactData(data);                  // Call the function to display the contact data
+        });
+}
+
+function homeLink() {
+    window.open("index.html", "_self");                // Redirect to the home page when the home link is clicked
 }
